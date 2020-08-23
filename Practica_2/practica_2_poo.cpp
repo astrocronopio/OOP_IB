7dd3eb7541c333a24e2057ba5117a0d376348ead
astrocronopio@hotmail.com <Evelyn  Coronel>
2020-08-23 11:04:12 -0300

    Terminé el ejercicio para entregar

--- Practica_2/practica_2_poo.cpp
+++ Practica_2/practica_2_poo.cpp
@@ -12,40 +12,83 @@ Escrito por Evelyn Coronel
 class Stack
 {
 	char *v;
-	int top;
-	int max_size;
+	const char *name;
+	int top, max_size;
 
 public:
-	Stack(int size);
+	Stack(int size, const char* name);
 	~Stack();
 
-	class Underflow{};
-	class Overflow{};
-	class Bad_size{};
+	class Underflow{ };
+	class Overflow{ };
+	class Bad_size{ };
 
 	void  push(char c);
 	char pop();
-	
+
+	friend std::ostream &operator<<(std::ostream &os, const Stack &stack_dummy)
+	{	if (stack_dummy.top==0) os<<"\nEl stack  \""<<stack_dummy.name<<"\" está vacío.\n\n";
+		else {
+			os<<  "\nImprimiendo el stack \""<<stack_dummy.name << "\":\n__Stack__\n\n";
+			for (int i = stack_dummy.top-1; i >= 0 ; i--)
+				os<<"Orden "<< i+1 <<"/"<<stack_dummy.max_size<<":"<< stack_dummy.v[i]<< "\n";
+			os<<"_________\n\n";}
+		return os;	
+	}
 };
 
 void Stack::push(char c)
 {
-	
+	if (top == max_size) throw Overflow{};
+	v[top++] = c;
+	std::cout << "Pushing \'" << c << "\' a \""<< name <<"\". ";
+	std::cout << " Quedan " << max_size - top <<" de "<<max_size << " lugares.\n";
 }
 
 char Stack::pop()
 {
-
+	if( top == 0) throw Underflow{};
+	top--;
+	std::cout << "Poping \'" << v[top] <<"\' de \""<< name <<"\" \n";
+	return v[top];
 }
 
-Stack::~Stack()
-{
+Stack::~Stack() {delete [] v;}
 
+Stack::Stack(int s, const char* name_init){
+	top=0;
+	if (s<=0 || s>1000) throw Stack::Bad_size{};
+	max_size = s;
+	name = name_init;
+	std::cout<<"Se creó el stack \""<<name<<"\" con lugar para "<<max_size<<" elementos.\n";
+
+	v= new char[s];
 }
-
 
 int main(int argc, char const *argv[])
-{ 
-	/* code */
+{
+	Stack s_var2(10, "primer stack");
+
+	s_var2.push('1');
+	s_var2.push('2');
+	s_var2.push('3');
+
+	std::cout<<s_var2;
+
+	s_var2.pop()  ;
+	s_var2.pop()  ;
+
+	std::cout<<s_var2;
+
+
+	Stack s_var1(2, "segundo stack");
+	std::cout << s_var1;
+	s_var1.push('1');
+	s_var1.push('2');
+
+	s_var1.push('F');
+
+	//Stack s_var3(2, "tercer stack");
+
 	return 0;
 }

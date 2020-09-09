999111a3deee279d747904bd0a4a0d2e06ca3d7a
astrocronopio@hotmail.com <Evelyn  Coronel>
2020-09-09 11:02:25 -0300

    Cambios a la entrega de la practica  5

--- Practica_5/ejer6_v1.cpp
+++ Practica_5/ejer6_v1.cpp
@@ -1,4 +1,3 @@
-
 /*
 Ejercicio 6 de la guia 5 de la materia de 
 OPP C++ del Instituto Balseiro, Septiembre 2020
@@ -37,30 +36,37 @@ public:
     ~ejer6_C();
 };
 
-ejer6_C::ejer6_C(){   
+ejer6_C::ejer6_C()
+{   
     value=1;
     std::cout<<"\nCreamos un objeto de la Clase Principal ejer6_C."<< std::endl;
 }
 
-ejer6_C::~ejer6_C(){
+ejer6_C::~ejer6_C()
+{
     std::cout<<"\nDestruimos un objeto de la Clase Principal ejer6_C con value="<< value<<std::endl;
 }
 
-ejer6_B::ejer6_B(){
+ejer6_B::ejer6_B()
+{
     valueB=0;
     std::cout<<"\nCreamos un objeto de la Classe ejer6_B."<< std::endl;
+    valueB+=1;
 }
 
-ejer6_B::~ejer6_B(){   
+ejer6_B::~ejer6_B()
+{   
     std::cout<<"\nDestruimos un objeto de la Classe ejer6_B con value="<< valueB<<std::endl;
 }
     
-ejer6_A::ejer6_A(){   
+ejer6_A::ejer6_A()
+{   
     valueA=0;
     std::cout<<"\nCreamos un objeto de la Classe ejer6_A"<< std::endl;
 }
 
-ejer6_A::~ejer6_A() {
+ejer6_A::~ejer6_A()
+{
     std::cout<<"\nDestruimos un objeto de la Classe ejer6_A con value="<< valueA <<std::endl;
 }
 
@@ -68,5 +74,12 @@ ejer6_A::~ejer6_A() {
 int main(int argc, char const *argv[])
 {   std::cout<<"\nSolo la clase principal: \n";
     ejer6_C prueba;
+
+    // std::cout<<"\nSolo la clase nesteada B: \n";
+    // ejer6_A::ejer6_B pruebaB;
+
+    // std::cout<<"\nSolo la clase nesteada C: \n";
+    // ejer6_A::ejer6_C pruebaC;
+
     return 0;
 }
--- Practica_5/ejer8.cpp
+++ Practica_5/ejer8.cpp
@@ -13,26 +13,60 @@ class Manager
 {
 private:
     int valor;
-    Manager();
-
+    static Manager* objeto;
+    
+    /////Privatizamos todo
+    Manager(){};
+    Manager(const Manager& dummy){};
+    void operator=(Manager const&){};
+    /////
 public:
-    static Manager* get_Instance();
+    
+    static Manager* get_Instance(){
+        if (objeto==nullptr) 
+        {
+            objeto = new Manager;
+        //return dummy ;
+        }
+        return objeto;
+    };
+    
+    int getManager() {return this -> valor; }
+    void setManager(int dato) { this -> valor = dato; }
+
+    void print(){std::cout<<"Direccion: "<<(void *) objeto<< std::endl;}
+
 };
 
 
-Manager* Manager::get_Instance()
-{
-    Manager* dummy ;
-    return dummy ;
-}
-
 /* ---------------------------------------- */
+
+Manager* Manager::objeto = 0; //Por alguna razón lo tengo que inicializar AFUERA o sino no la ve
 
 int main(int argc, char const *argv[])
 {   
-    //Manager intento; NO funciona
-    Manager* intento2;
-    Manager * puntero = Manager::get_Instance();
     
+    //Manager intento; NO funciona
+    //Manager intento= *Manager::get_Instance(); //NO funciona
+    
+    /*NO  funcionan
+
+    Manager* intento2; 
+    intento2->print(); 
+    intento2->setManager(4); //NO  funcionan
+    std::cout<<"getter output: "<<intento2->getManager()<<"\n"; */
+    
+    Manager * puntero = puntero->get_Instance();
+    puntero->print();
+    puntero->setManager(42);
+    std::cout<<"getter output: "<<puntero->getManager()<<"\n";
+
+    std::cout<<"\nIntento crear otra instancia: \n";
+
+    Manager * puntero2 = puntero->get_Instance();
+    puntero2->print();
+    std::cout<<"getter output 2: "<<puntero2->getManager()<<"\n";
+    
+    std::cout<<"\nPero finalmente es la misma dirección y objeto. \n";
     return 0;
 }
